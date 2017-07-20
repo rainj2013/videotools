576c358bb1eeee743f78ab2af6da2aaf1cec211a
rainj2013 <yangyujian25@gmail.com>
2017-07-20 10:28:37 +0800

    添加一些接口文档说明

--- src/main/java/top/rainj2013/controller/DownloadController.java
+++ src/main/java/top/rainj2013/controller/DownloadController.java
@@ -1,6 +1,8 @@
 package top.rainj2013.controller;
 
 import com.google.common.collect.Maps;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -26,7 +28,12 @@ public class DownloadController {
     @Autowired
     private DownloadService downloadService;
 
+    //swagger api config start
     @ApiOperation(value = "下载视频", notes = "调用you-get/tget工具下载视频，支持多个主流视频网站和磁性链接", httpMethod = "POST")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "form", value = "下载任务", required = true, dataType = "DownloadForm"),
+            @ApiImplicitParam(name = "token", value = "令牌", required = true, dataType = "String", paramType = "path")})
+    //swagger api config end
     @RequestMapping(value = "", method = RequestMethod.POST)
     @ResponseBody
     @LoginCheck
