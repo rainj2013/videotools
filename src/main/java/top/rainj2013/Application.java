69100c4926272f586d1aed527fcaaf3ef8ead2b5
rainj2013 <yangyujian25@gmail.com>
2017-07-27 20:03:45 +0800

    添加首页影射

--- src/main/java/top/rainj2013/Application.java
+++ src/main/java/top/rainj2013/Application.java
@@ -2,6 +2,8 @@ package top.rainj2013;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Author:  rainj2013
@@ -9,8 +11,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Date:  17-07-15
  */
 @SpringBootApplication
+@Controller
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "redirect:/swagger-ui.html#/";
     }
 }
